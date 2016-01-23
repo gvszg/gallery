936a293d10fdf5261d56ab8ff2bc3266a500f1d9
gvszg <zetaentranceserious@gmail.com>
2016-01-23 17:22:05 +0800

    Add url regexp check, install meta request gem for nice debug tool

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,13 +1,19 @@
 class WebsitesController < ApplicationController
   def index
-    if params[:url]
-      @website = Website.find_by(url: params[:url])
-      if @website
-        @photos = @website.photos
+    if params[:url] &&
+      if url_correct?(params[:url])
+        @website = Website.find_by(url: params[:url])
+        
+        if @website
+          @photos = @website.photos
+        else
+          @website = Website.create!(url: params[:url])
+          @photos = create_photos_from_url(@website)
+        end
       else
-        @website = Website.create!(url: params[:url])
-        @photos = create_photos_from_url(@website)        
-      end    
+        flash[:notice] = "網址格式有誤，請檢查是否為正確網址！"
+        redirect_to :back
+      end      
     end
   end
 
@@ -21,25 +27,28 @@ class WebsitesController < ApplicationController
     photos_doc = doc.css("img")
     images = []
     
-    photos_doc.each do |i|
-      images << i.attr('src')
+    photos_doc.each do |i|      
+      images << i.attr('src') if i.attr('src')
     end
     
     pics = images.map do |url|
-      if url.include?("http")
+      if url && url.include?("http")
         url
       elsif url =~ /^\/\//
         url.sub!(/^\/\//, "http://")
       else
-        protocol_domain + url
-      end
+        protocol_domain + '/' + url
+      end    
     end
     
-
     pics.each do |pic|
       website.photos.create!(image: pic)
     end
 
     return website.photos
+  end
+
+  def url_correct?(url)
+    url =~ /^(http|https):\/\/.+?$/i ? true : false
   end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -10,31 +10,4 @@
 
 class Website < ActiveRecord::Base
   has_many :photos
-
-  # def paese_img_src
-  #   # uri = 
-  #   protocol_domain = %Q(#{uri.scheme}//#{uri.host})
-  #   content = open(link).read
-  #   doc = Nokogiri::HTML(content).read
-  #   photos_doc = doc.search("img")
-  #   images = []
-
-  #   photos_doc.each do |i|
-  #     images << i.attr('src')
-  #   end
-
-  #   pics = images.map do |url|
-  #     if url.include?("http")
-  #       url
-  #     elsif url =~ /^\/\//
-  #       url.sub!(/^\/\//, "http://")
-  #     else
-  #       protocol_domain + url
-  #   end
-
-  #   website = Website.find_by(url: link)
-  #   pics.each do |pic|
-  #     website.photos.create!(image: pic)
-  #   end
-  # end  
 end
